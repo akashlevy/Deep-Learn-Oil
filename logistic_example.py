4fd8001dbff54c34bb99134f032b58b6038ff9a2
Akash Levy <akashlevy@gmail.com>
2015-06-19 16:51:07 -0400

    Added a plot to logistic regression example. Finished it.

--- logistic_example.py
+++ logistic_example.py
@@ -4,24 +4,22 @@ import theano.tensor as T
 from matplotlib import pyplot as plt
 rng = numpy.random
 
+# Parameters
 N = 400
 feats = 784
 D = (rng.randn(N, feats), rng.randint(size=N, low=0, high=2))
-training_steps = 10000
+training_steps = 500
 
 # Declare Theano symbolic variables
 x = T.matrix("x")
 y = T.vector("y")
 w = theano.shared(rng.randn(feats), name="w")
 b = theano.shared(0., name="b")
+
+# Initial model
 print "Initial model:"
 print w.get_value(), b.get_value()
 print
-fig = plt.figure(1)
-graph = fig.add_subplot(111)
-graph.plot(range(T.shape(w)), w)
-graph.plot(range(T.shape(w)+1), T.zeros_like(w).append(b))
-fig.show()
 
 # Construct Theano expression graph
 p_1 = 1 / (1 + T.exp(-T.dot(x, w) - b))   # Probability that target = 1
@@ -37,28 +35,28 @@ train = theano.function(
           inputs=[x,y],
           outputs=[prediction, xent],
           updates=((w, w - 0.1 * gw), (b, b - 0.1 * gb)))
+
+# Prediction function
 predict = theano.function(inputs=[x], outputs=prediction)
 
 # Train
 for i in range(training_steps):
     pred, err = train(D[0], D[1])
 
+# Final model output
 print "Final model:"
 print w.get_value(), b.get_value()
 print
 print "target values for D:"
 print D[1]
+print
 print "prediction on D:"
 print predict(D[0])
 
-fig2 = plt.figure(2)
-graph2 = fig2.add_subplot(111)
-graph2.plot(w)
-graph2.plot(T.zeros_like(w).append(b))
-fig2.show()
-
-fig3 = plt.figure(3)
-graph3 = fig3.add_subplot(111)
-graph3.plot(D[1])
-graph3.plot(predict(D[0]))
-fig3.show()
+# Plot to see similarities and differences
+fig = plt.figure(1)
+graph = fig.add_subplot(111)
+graph.plot(range(400), D[1], "r-o", label="Target")
+graph.plot(range(400), predict(D[0]), "g-o", label="Prediction")
+plt.legend()
+plt.show()
