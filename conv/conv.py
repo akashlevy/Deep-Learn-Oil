497f71618a1ddd14713906ea96e8f9fa941cd0a7
Akash Levy <akashlevy@gmail.com>
2015-07-14 17:21:23 -0400

    Modified conv stuff

--- conv/conv.py
+++ conv/conv.py
@@ -14,13 +14,13 @@ x = T.matrix('x')
 y = T.matrix('y')
 
 # Make neural network
-layer0 = conv1d.ConvPoolLayer(rng, x, input_length=36, batch_size=25, filters=10, filter_length=5, poolsize=4)
-print layer0.input_size, layer0.output_size, layer0.filter_size
-layer1 = conv1d.ConvPoolLayer(rng, layer0.output, input_number=10, input_length=8, batch_size=25, filters=10, filter_length=4, poolsize=5)
-print layer1.input_size, layer1.output_size, layer1.filter_size
-layer2 = conv1d.FullyConnectedLayer(rng, layer1.output.flatten(), n_in=100, n_out=12)
-network = conv1d.ConvNetwork(datasets, x, y, batch_size=25, layers=[layer0, layer1, layer2], learning_rate=0.1)
+batch_size = 50
+
+layer0 = conv1d.ConvPoolLayer(rng, x, input_length=36, batch_size=batch_size, filters=10, filter_length=5, poolsize=4)
+layer1 = conv1d.ConvPoolLayer(rng, layer0.output, input_number=layer0.output_shape[1], input_length=layer0.output_shape[3], batch_size=batch_size, filters=10, filter_length=4, poolsize=5)
+layer2 = conv1d.FullyConnectedLayer(rng, layer1.output.flatten(2), n_in=layer1.output_shape[1]*layer1.output_shape[3], n_out=12)
+network = conv1d.ConvNetwork(datasets, x, y, batch_size=batch_size, layers=[layer0, layer1, layer2], learning_rate=0.1)
 
 # Train and test neural network
-network.train(10)
+network.train(100)
 network.test()
