8e24c1aa344a771d17a10dc36cc4248f2acbee59
Akash Levy <akashlevy@gmail.com>
2015-07-14 17:21:23 -0400

    Modified conv stuff

--- conv/conv1d.py
+++ conv/conv1d.py
@@ -69,17 +69,17 @@ class ConvNetwork(object):
         index = T.lscalar()
         givens = {x: self.train_set_x[index * batch_size: (index + 1) * batch_size], y: self.train_set_y[index * batch_size: (index + 1) * batch_size]}
         updates = [(param_i, param_i - learning_rate * grad_i) for param_i, grad_i in zip(self.params, T.grad(self.cost, self.params))]
-        self.train = theano.function([index], self.cost, updates=updates, givens=givens)
+        self.train_model = theano.function([index], self.cost, updates=updates, givens=givens)
         givens = {x: self.valid_set_x[index * batch_size: (index + 1) * batch_size], y: self.valid_set_y[index * batch_size: (index + 1) * batch_size]}
-        self.validate = theano.function([index], self.cost, givens = givens)
+        self.validate_model = theano.function([index], self.cost, givens = givens)
         givens = {x: self.test_set_x[index * batch_size: (index + 1) * batch_size], y: self.test_set_y[index * batch_size: (index + 1) * batch_size]}
-        self.test = theano.function([index], self.cost, givens=givens)
+        self.test_model = theano.function([index], self.cost, givens=givens)
         
     def train(self, train_steps): # EVENTUALLY INTRODUCE EARLY STOPPING
         """Train network"""
         for epoch in range(train_steps):
-            costs = [train_model(i) for i in xrange(self.n_train_batches)]
-            validation_losses = [validate(i) for i in xrange(self.n_valid_batches)]
+            costs = [self.train_model(i) for i in xrange(self.n_train_batches)]
+            validation_losses = [self.validate_model(i) for i in xrange(self.n_valid_batches)]
             print "Epoch {}    NLL {:.2}    %err in validation set {:.1%}".format(epoch + 1, np.mean(costs), np.mean(validation_losses))
             
     def test(self):
@@ -96,19 +96,19 @@ class ConvPoolLayer(object):
         assert (input_length - filter_length + 1) % poolsize == 0
         
         # Determine input, output, and filter tensor sizes
-        self.input_size = (batch_size, input_number, 1, input_length)
-        self.output_size = (batch_size, input_number*filters, 1, (input_length-filter_length+1)/poolsize)
-        self.filter_size = (filters, input_number, 1, filter_length)
+        self.input_shape = (batch_size, input_number, 1, input_length)
+        self.output_shape = (batch_size, input_number*filters, 1, (input_length-filter_length+1)/poolsize)
+        self.filter_shape = (filters, input_number, 1, filter_length)
         
         # Reshape input to input size and store
-        self.input = input.reshape(self.input_size)
+        self.input = input.reshape(self.input_shape)
         
         # Model parameters (weights and biases)
-        self.W = theano.shared(np.asarray(rng.uniform(low=-W_bound, high=W_bound, size=self.filter_size), dtype=theano.config.floatX), borrow=True)
-        self.b = theano.shared(value=np.zeros(filter_length, dtype=theano.config.floatX), borrow=True)
+        self.W = theano.shared(np.asarray(rng.uniform(low=-W_bound, high=W_bound, size=self.filter_shape), dtype=theano.config.floatX), borrow=True)
+        self.b = theano.shared(value=np.zeros(filters, dtype=theano.config.floatX), borrow=True)
 
         # Convolve input feature maps with filters
-        conv_out = conv.conv2d(self.input, self.W, image_shape=self.input_size, filter_shape=self.filter_size)
+        conv_out = conv.conv2d(self.input, self.W, image_shape=self.input_shape, filter_shape=self.filter_shape)
 
         # Downsample each feature map individually, using maxpooling
         pooled_out = downsample.max_pool_2d(input=conv_out, ds=(1, poolsize), ignore_border=True)
