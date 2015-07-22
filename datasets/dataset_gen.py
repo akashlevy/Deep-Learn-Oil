314453ffd22d30db6ccc3ff4835c63c640ea16a5
Akash Levy <akashlevy@gmail.com>
2015-07-22 16:59:14 -0400

    Updated dataset_gen.py's plotter, added prediction graphs (png and time lapse gifs generated by makegif.sh), updated nnet1d, scrapped model files, added tanh to nnet_functions.py

--- datasets/dataset_gen.py
+++ datasets/dataset_gen.py
@@ -72,14 +72,19 @@ def get_data():
 
 def preprocess_data(data):
     """Returns preprocessed version of the data"""
+    # Initialize dataset components
     train_x = []
     train_y = [] 
     valid_x = []
     valid_y = []
     test_x = []
     test_y = []
+    
+    # Shuffle wells
     well_names = data.keys()
     rnd.shuffle(well_names)
+    
+    # Go through wells and assign to datasets
     for well_index, well_name in enumerate(well_names):
         # Remove zeroed data points (push points together)
         if REMOVE_ZEROS:
@@ -93,48 +98,49 @@ def preprocess_data(data):
             oils = np.convolve(smooth_window, oils, mode="valid")
         
         # Make chunks
-        for i in xrange(0, len(oils), STEP_MONTHS):
+        for i in xrange(0, len(oils)-(IN_MONTHS+OUT_MONTHS), STEP_MONTHS):
+            # Split data into x, y, and chunk
             in_index = i
             out_index = i + IN_MONTHS
             end_index = i + IN_MONTHS + OUT_MONTHS
-            if end_index < len(oils):
-                chunk = oils[in_index:end_index]
-                chunk_x = oils[in_index:out_index]
-                chunk_y = oils[out_index:end_index]
-                
-                # Normalize data (skip chunk if standard deviation is 0)
-                if NORMALIZE_DATA and np.std(chunk) != 0:
-                    chunk_x = (chunk_x - np.mean(chunk))/np.std(chunk)
-                    chunk_y = (chunk_y - np.mean(chunk))/np.std(chunk)
-                
-                # Add chunk
-                if DIFFERENT_SITES:
-                    # Assign to dataset based on site name
-                    if name[:4] in TRAIN_SITES:
-                        train_x.append(chunk_x)
-                        train_y.append(chunk_y)
-                    elif name[:4] in VALID_SITES:
-                        valid_x.append(chunk_x)
-                        valid_y.append(chunk_y)
-                    elif name[:4] in TEST_SITES:
-                        test_x.append(chunk_x)
-                        test_y.append(chunk_y)
-                    else:
-                        print "Error: site %s not classified" % name
-                elif DIFFERENT_WELLS:
-                    # Assign to dataset based on well index
-                    if well_index < len(data)*6/8:
-                        train_x.append(chunk_x)
-                        train_y.append(chunk_y)
-                    elif well_index < len(data)*7/8:
-                        valid_x.append(chunk_x)
-                        valid_y.append(chunk_y)
-                    else:
-                        test_x.append(chunk_x)
-                        test_y.append(chunk_y)
+            chunk = oils[in_index:end_index]
+            chunk_x = oils[in_index:out_index]
+            chunk_y = oils[out_index:end_index]
+            
+            # Normalize data (skip chunk if standard deviation is 0)
+            if NORMALIZE_DATA and np.std(chunk) != 0:
+                chunk_x = (chunk_x - np.mean(chunk))/np.std(chunk)
+                chunk_y = (chunk_y - np.mean(chunk))/np.std(chunk)
+            
+            # Add chunk
+            if DIFFERENT_SITES:
+                # Assign to dataset based on site name
+                if name[:4] in TRAIN_SITES:
+                    train_x.append(chunk_x)
+                    train_y.append(chunk_y)
+                elif name[:4] in VALID_SITES:
+                    valid_x.append(chunk_x)
+                    valid_y.append(chunk_y)
+                elif name[:4] in TEST_SITES:
+                    test_x.append(chunk_x)
+                    test_y.append(chunk_y)
                 else:
-                    print "Error: choose a dataset assignment option"
-                    
+                    print "Error: site %s not classified" % name
+            elif DIFFERENT_WELLS:
+                # Assign to dataset based on well index
+                if well_index < len(data)*6/8:
+                    train_x.append(chunk_x)
+                    train_y.append(chunk_y)
+                elif well_index < len(data)*7/8:
+                    valid_x.append(chunk_x)
+                    valid_y.append(chunk_y)
+                else:
+                    test_x.append(chunk_x)
+                    test_y.append(chunk_y)
+            else:
+                print "Error: choose a dataset assignment option"
+
+    # Make datasets
     train_set = (np.array(train_x), np.array(train_y))
     valid_set = (np.array(valid_x), np.array(valid_y))
     test_set = (np.array(test_x), np.array(test_y))
@@ -154,24 +160,19 @@ def plot_chunks(datasets):
             plt.ylabel("Production", fontsize=15)
             
             # Plot the predictions as a green line with round markers
-            graph.plot(np.append(chunk[0], chunk[1]), "g-o", label="Predicted Output")
+            prediction = np.append(chunk[0], chunk[1])
+            graph.plot(prediction, "g-o", label="Prediction")
     
-            # Plot the data as a red line with round markers
-            graph.plot(chunk[0], "r-o", label="Oil Output")
+            # Plot the past as a red line with round markers
+            past = chunk[0]
+            graph.plot(past, "r-o", label="Past")
     
             # Add legend and display plot
-            plt.legend()
+            plt.legend(loc="upper left")
             plt.show()
-
-   
-def load_data(seed, remove_zeros, smooth_data, normalize_data, smooth_len):
-    """Return datasets: allows this function to be called from other modules"""
-    rnd.seed(SEED)
-    REMOVE_ZEROS = remove_zeros
-    SMOOTH_DATA = smooth_data
-    NORMALIZE_DATA = normalize_data
-    SMOOTH_LEN = smooth_len
-    return generate_data_sets(preprocess_data(get_data()))
+            
+            # Close the plot
+            plt.close(fig)
 
 
 if __name__ == '__main__':
--- nnet1d/fcn.py
+++ nnet1d/fcn.py
@@ -1,97 +1,13 @@
-"""Testing nnet1d's convolutional neural network on QRI oil data"""
+"""Test nnet1d"""
 
-import copy
-import matplotlib.pyplot as plt
-import numpy as np
-import theano.tensor as T
 from nnet1d import NNet1D
-from nnet_functions import relu, abs_error_cost
-
-# List of models
-models = []
+from nnet_functions import abs_error_cost, tanh
 
 # Fully connected model 1
-models.append(NNet1D(datafile="../datasets/qri.pkl.gz", seed=42, batch_size=300,
-                     learning_rate=0.01, momentum=0, cost_fn=abs_error_cost))
-# models[-1].add_fully_connected_layer(output_length=32, activ_fn=T.tanh)
-# models[-1].add_fully_connected_layer(output_length=28, activ_fn=T.tanh)
-# models[-1].add_fully_connected_layer(output_length=24, activ_fn=T.tanh)
-# models[-1].add_fully_connected_layer(output_length=20, activ_fn=T.tanh)
-# models[-1].add_fully_connected_layer(output_length=16, activ_fn=T.tanh)
-models[-1].add_fully_connected_layer()
-models[-1].build()
-
-# Early stopping parameters
-patience = 15
-min_epochs = 100
-max_epochs = 50000
-
-# Train models
-for model in models:
-    # Early stopping bests
-    best_model = None
-    best_validation_error = np.inf
-    best_validation_index = 0
-    
-    # Train model
-    for index in xrange(max_epochs):
-        training_error, validation_error = model.train()
-        
-        # Only check bests if past min_epochs
-        if index > min_epochs:
-            # If lower validation error, record new best
-            if validation_error < best_validation_error:
-                best_model = copy.deepcopy(model)
-                best_validation_error = validation_error
-                best_validation_index = index
-                
-            # If patience exceeded, done training
-            if best_validation_index + patience < index:
-                break
-        
-        # Print epoch, training error and validation error for progress record
-        print "(%s, %s, %s)" % (index + 1, training_error, validation_error)
-    
-    # Test neural network
-    print "Testing error = %s\n" % model.test_error()
-    
-    # Replace old model with best model
-    model = best_model
-
-# Save models
-import gzip, cPickle
-with gzip.open("models/simple_model.pkl.gz", "wb") as file:
-    file.write(cPickle.dumps(models))
-
-# Load model
-import gzip, cPickle
-with gzip.open("models/simple_model.pkl.gz", "rb") as file:
-    models = cPickle.load(file)
-
-# Load test data and make prediction
-x = models[-1].test_set_x.get_value()
-y = models[-1].test_set_y.get_value()
-prediction = models[-1].output(x)
-
-for chunk in zip(x, y, prediction):
-    # Create a figure and add a subplot with labels
-    fig = plt.figure(1)
-    graph = fig.add_subplot(111)
-    fig.suptitle("Chunk Data", fontsize=25)
-    mean_abs_error = abs_error_cost(chunk[1], chunk[2]).eval()
-    plt.title("Mean Abs Error: %f" % mean_abs_error, fontsize=10)
-    plt.xlabel("Month", fontsize=15)
-    plt.ylabel("Production", fontsize=15)
-    
-    # Plot the generated predictions as a blue line with round markers
-    graph.plot(np.append(chunk[0], chunk[2]), "b-o", label="Generated Output")
-    
-    # Plot the actual predictions as a green line with round markers
-    graph.plot(np.append(chunk[0], chunk[1]), "g-o", label="Actual Output")
-
-    # Plot the data as a red line with round markers
-    graph.plot(chunk[0], "r-o", label="Oil Output")
-
-    # Add legend and display plot
-    plt.legend(loc="upper left")
-    plt.show()
+model = NNet1D(datafile="../datasets/qri.pkl.gz", seed=42, batch_size=200, learning_rate=0.01, momentum=0, cost_fn=abs_error_cost)
+# model.add_conv_pool_layer(filters=24, filter_length=11, poolsize=2, activ_fn=tanh)
+# model.add_conv_pool_layer(filters=24, filter_length=12, poolsize=2, activ_fn=tanh)
+model.add_fully_connected_layer()
+model.build()
+model.train_early_stopping(patience=15, min_epochs=100)
+# model.plot_test_predictions(display_figs=False, save_figs=True, output_folder="conv_images")
