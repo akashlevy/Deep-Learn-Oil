Scrapped all the images, fixed preprocessing error with biased normalization, updated qri.pkl.gz, added features and fixed bugs in nnet1d

nnet1d/layers1d.py
@@ -1,71 +1,92 @@
 """Layer classes of a 1-D neural network"""
 
+import matplotlib.pyplot as plt
 import numpy as np
 import theano
 import theano.tensor as T
 from theano.tensor.signal import downsample
 from theano.tensor.nnet import conv
-from nnet_functions import relu, sqr_error_cost
+from nnet_functions import tanh, abs_error_cost
 
 
-class ConvPoolLayer(object):
+class Layer(object):
+    """Layer of a 1-D neural network"""
+    def __init__(self, input, input_length, activ_fn):
+        """Store required layer attributes"""
+        self.input = input
+        self.input_length = input_length
+        self.activ_fn = activ_fn
+
+
+class ConvPoolLayer(Layer):
     """Convolutional layer of a 1-D neural network"""
-    def __init__(self, rng, input, input_length, batch_size, filters,
-                 filter_length, input_number=1, poolsize=1, activ_fn=relu,
-                 W_bound=0.1):
+    def __init__(self, rng, input, input_length, filters, filter_length,
+                 input_number=1, poolsize=1, activ_fn=tanh, W_bound=0.1):
         """Initialize layer"""
         # Make sure that convolution output is evenly divisible by poolsize
         assert (input_length - filter_length + 1) % poolsize == 0
         
-        # Determine input, output, and filter tensor sizes
-        conv_output_size = (input_length-filter_length+1)/poolsize
-        self.input_shape = (batch_size, input_number, 1, input_length)
-        self.output_shape = (batch_size, filters, 1, conv_output_size)
+        # Determine input and output length as well as filter shape
+        self.output_length = (input_length - filter_length + 1) / poolsize
         self.filter_shape = (filters, input_number, 1, filter_length)
         
-        # Store poolsize and activation function
+        # Store input, input length, activation function, and poolsize
+        super(ConvPoolLayer,self).__init__(input, input_length, activ_fn)
         self.poolsize = poolsize
-        self.activ_fn = activ_fn
-        
-        # Reshape input to input size and store
-        self.input = input.reshape(self.input_shape)
         
         # Model parameters (weights and biases)
-        filts = rng.uniform(low=-W_bound, high=W_bound, size=self.filter_shape)
         dtype = theano.config.floatX
+        filts = rng.uniform(low=-W_bound, high=W_bound, size=self.filter_shape)
         self.W = theano.shared(np.asarray(filts, dtype=dtype), borrow=True)
         self.b = theano.shared(np.zeros(filters, dtype=dtype), borrow=True)
 
         # Convolve input feature maps with filters
-        conv_out = conv.conv2d(self.input, self.W,
-                               image_shape=self.input_shape,
-                               filter_shape=self.filter_shape)
+        conv_out = conv.conv2d(self.input, self.W, None, self.filter_shape)
 
         # Downsample each feature map individually, using maxpooling
-        pooled_out = downsample.max_pool_2d(input=conv_out,
-                                            ds=(1, self.poolsize),
-                                            ignore_border=True)
+        pooled_out = downsample.max_pool_2d(input=conv_out, ds=(1, poolsize))
         
         # Store output
-        bias = self.b.dimshuffle('x', 0, 'x', 'x')
-        self.output = self.activ_fn(pooled_out + bias)
+        self.output = activ_fn(pooled_out + self.b.dimshuffle('x',0,'x','x'))
 
         # Store parameters of this layer
         self.params = [self.W, self.b]
 
+    def __repr__(self):
+        """Return string representation of ConvPoolLayer"""
+        format_line = "ConvPoolLayer(rng, input, %s, %s, %s, "
+        format_line += "input_number=%s, poolsize=%s, activ_fn=%s)"
+        activ_fn_name = self.activ_fn.__name__ if self.activ_fn else "None"
+        return format_line % (self.input_length, self.filter_shape[0],
+                              self.filter_shape[3], self.filter_shape[1],
+                              self.poolsize, activ_fn_name)
+    
+    def __str__(self):
+        """Return string representation of ConvPoolLayer"""
+        return self.__repr__()
 
-class FullyConnectedLayer(object):
+    def plot_filters(self, cmap="gray"):
+        """Plot the filters"""
+        filters = self.W.get_value(borrow=True)
+        new_shape = (-1, filters.shape[3])
+        filters = np.resize(filters, new_shape)
+        print filters
+        fig = plt.figure(1)
+        graph = fig.add_subplot(111)
+        mat = graph.matshow(filters, cmap=cmap, interpolation="none")
+        fig.colorbar(mat)
+        plt.show()
+
+
+class FullyConnectedLayer(Layer):
     """Fully connected layer of a 1-D neural network"""
-    def __init__(self, rng, input, input_length, output_length, batch_size,
-                 cost_fn=sqr_error_cost, activ_fn=None, W_bound=0.1):
+    def __init__(self, rng, input, input_length, output_length, activ_fn=None,
+                 cost_fn=abs_error_cost, W_bound=0.1):
         """Initialize fully connected layer"""
-        # Determine input and output tensor sizes
-        self.input_shape = (batch_size, input_length)
-        self.output_shape = output_length
-        
-        # Store input and cost function
-        self.input = input
+        # Store layer parameters, cost function, output length
+        super(FullyConnectedLayer,self).__init__(input, input_length, activ_fn)
         self.cost_fn = cost_fn
+        self.output_length = output_length
         
         # Model parameters (weights and biases)'
         weight_size = (input_length, output_length)
@@ -78,9 +99,31 @@ class FullyConnectedLayer(object):
         # Store output and params of this layer
         self.output = T.dot(input, self.W) + self.b
         if activ_fn is not None:
-            self.output = activ_fn(self.output)
+            self.output = self.activ_fn(self.output)
         self.params = [self.W, self.b]
 
+    def __repr__(self):
+        """Return string representation of FullyConnectedLayer"""
+        format_line = "FullyConnectedLayer(rng, input, %s, %s, activ_fn=%s, "
+        format_line += "cost_fn=%s)"
+        activ_fn_name = self.activ_fn.__name__ if self.activ_fn else "None"
+        cost_fn_name = self.cost_fn.__name__ if self.cost_fn else "None"
+        return format_line % (self.input_length, self.output_length,
+                              activ_fn_name, cost_fn_name)
+    
+    def __str__(self):
+        """Return string representation of FullyConnectedLayer"""
+        return self.__repr__()
+    
     def cost(self, y):
         """Return the cost associated with the output"""
         return self.cost_fn(y, self.output)
+        
+    def plot_weights(self, cmap="gray"):
+        """Plot the weight matrix"""
+        weights = self.W.get_value(borrow=True)
+        fig = plt.figure(1)
+        graph = fig.add_subplot(111)
+        mat = graph.matshow(weights, cmap=cmap, interpolation="none")
+        fig.colorbar(mat)
+        plt.show()

nnet1d/nnet1d.py
@@ -10,7 +10,7 @@ import theano
 import theano.tensor as T
 import time
 from layers1d import ConvPoolLayer, FullyConnectedLayer
-from nnet_functions import abs_error_cost, tanh
+from nnet_fns import abs_error_cost, tanh
 
 
 # Configure floating point numbers for Theano
@@ -35,9 +35,9 @@ class NNet1D(object):
         # Initialize layers in the neural network
         self.layers = []
         
-        # Input and output matrices (2D)
-        self.x = T.matrix('x')
-        self.y = T.matrix('y')
+        # Input and output tensors
+        self.x = T.matrix('x', theano.config.floatX)
+        self.y = T.matrix('y', theano.config.floatX)
         
         # Split into training, validation, and testing datasets
         datasets = NNet1D.load_data(datafile)
@@ -59,37 +59,38 @@ class NNet1D(object):
 
     def add_conv_pool_layer(self, filters, filter_length, poolsize,
                             activ_fn=tanh):
-        """Add a convolutional layer to the network"""
+        """Add a convolutional layer to the network"""        
         # If first layer, use x as input
         if len(self.layers) == 0:
-            input = self.x
+            new_shape = (self.batch_size, 1, 1, self.n_in)
+            input = self.x.reshape(new_shape)
             input_number = 1
             input_length = self.n_in
         
         # If previous layer is convolutional, use its output as input
         elif isinstance(self.layers[-1], ConvPoolLayer):
             input = self.layers[-1].output
-            input_number = self.layers[-1].output_shape[1]
-            input_length = self.layers[-1].output_shape[3]
+            input_number = self.layers[-1].filter_shape[0]
+            input_length = self.layers[-1].output_length
         
         # If previous layer is fully connected, use its output as input
         elif isinstance(self.layers[-1], FullyConnectedLayer):
-            input = self.layers[-1].output
+            new_shape = (1, 1, 1, self.layers[-1].output_shape[0])
+            input = self.layers[-1].output.reshape(new_shape)
             input_number = 1
-            input_length = self.layers[-1].output_shape
+            input_length = self.layers[-1].output_shape[0]
         
         # Otherwise raise error
         else:
             raise TypeError("Invalid previous layer")
             
         # Add the layer
-        layer = ConvPoolLayer(self.rng, input, input_length, self.batch_size,
-                              filters, filter_length, input_number, poolsize,
-                              activ_fn)
+        layer = ConvPoolLayer(self.rng, input, input_length, filters,
+                              filter_length, input_number, poolsize, activ_fn)
         self.layers.append(layer)
 
     def add_fully_connected_layer(self, output_length=None, activ_fn=None):
-        """Add a fully connected layer to the network"""
+        """Add a fully connected layer to the network"""        
         # If output_length is None, use self.n_out
         if output_length is None:
             output_length = self.n_out
@@ -102,13 +103,13 @@ class NNet1D(object):
         # If previous layer is convolutional, use its flattened output as input
         elif isinstance(self.layers[-1], ConvPoolLayer):
             input = self.layers[-1].output.flatten(2)
-            output_shape = self.layers[-1].output_shape
-            input_length = self.layers[-1].filter_shape[1] * output_shape[3]
+            input_length = self.layers[-1].filter_shape[1]
+            input_length *= self.layers[-1].output_length
         
         # If previous layer is fully connected, use its output as input
         elif isinstance(self.layers[-1], FullyConnectedLayer):
             input = self.layers[-1].output
-            input_length = self.layers[-1].output_shape
+            input_length = self.layers[-1].output_length
         
         # Otherwise raise error
         else:
@@ -116,15 +117,14 @@ class NNet1D(object):
             
         # Add the layer
         layer = FullyConnectedLayer(self.rng, input, input_length,
-                                    output_length, self.batch_size,
-                                    self.cost_fn, activ_fn)
+                                    output_length, activ_fn, self.cost_fn)
         self.layers.append(layer)
 
     def build(self):
-        """Build the neural network from the given layers"""
+        """Build the neural network from the given layers"""        
         # Last layer must be fully connected and produce correct output size
         assert isinstance(self.layers[-1], FullyConnectedLayer)
-        assert self.layers[-1].output_shape == self.n_out
+        assert self.layers[-1].output_length == self.n_out
         
         # Cost function is last layer's output cost
         self.cost = self.layers[-1].cost(self.y)
@@ -173,7 +173,7 @@ class NNet1D(object):
         output = self.layers[-1].output
         
         # Make Theano output function
-        self.output_function = theano.function([x], output, givens=givens)
+        self.output = theano.function([x], output, givens=givens)
 
     def gradient_updates_momentum(self, params):
         """Return the updates necessary to implement momentum"""
@@ -227,27 +227,12 @@ class NNet1D(object):
         with gzip.open(filename, "rb") as file:
             return cPickle.load(file)
 
-    def output(self, x):
-        """Return output from an input to the network"""
-        # Copy x to own its data
-        x = np.copy(x)
-        
-        # Store x's initial size
-        x_size = x.shape[0]
-        
-        # Resize x
-        x.resize(self.batch_size, self.n_in)
-        
-        # Return the output in x's initial size
-        return self.output_function(x)[:x_size]
-
     def plot_test_predictions(self, display_figs=True, save_figs=False,
                               output_folder="images", output_format="png"):
         """Plots the predictions for the first batch of the test set"""
         # Load test data and make prediction
-        batch_size = self.batch_size
-        x = self.test_set_x.get_value()
-        y = self.test_set_y.get_value()
+        x = self.test_set_x.get_value(borrow=True)
+        y = self.test_set_y.get_value(borrow=True)
         prediction = self.output(x)
 
         # Plot each chunk with its prediction
@@ -315,7 +300,7 @@ class NNet1D(object):
     def print_output_graph(self, outfile, format="svg"):
         """Print computational graph for producing output to filename in
         specified format"""
-        return theano.printing.pydotprint(self.output_function, format=format,
+        return theano.printing.pydotprint(self.output, format=format,
                                           outfile=outfile)
 
     def save_model(self, filename):
@@ -375,7 +360,8 @@ class NNet1D(object):
                 print "(%s, %s, %s)" % errors
         
         # Replace old model with best model
-        self.__dict__ = best_model.__dict__
+        if best_model is not None:
+            self.__dict__ = best_model.__dict__
         
         # Stop timer
         end_time = time.time()