226d097c7a519e14f8a2a182edd6f0f4736c994f
Akash Levy <akashlevy@gmail.com>
2015-07-27 14:31:14 -0400

    Add images, add examples, rename some papers to have shorter file names, fix some issues in nnet1d, add recurrent neural networks to nnet1d

---
+++ fcn.py
@@ -0,0 +1,21 @@
+"""Test fully connected neural network"""
+
+import nnet1d
+
+# Create model
+model = nnet1d.NNet1D(datafile="datasets/qri.pkl.gz", seed=42, batch_size=50,
+                      learning_rate=0.01, momentum=0.99,
+                      cost_fn=nnet1d.abs_error_cost)
+
+# Add layers and connect them
+model.add_fully_connected_layer(output_length=24, activ_fn=nnet1d.relu)
+model.add_fully_connected_layer()
+model.build()
+
+# Train until validation error does not improve
+model.train_early_stopping()
+
+# Save and plot data
+model.save_model("fcn.mdl")
+model.plot_train_valid_error()
+model.plot_test_predictions()
