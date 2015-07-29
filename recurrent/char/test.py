11da0178a02e8d3219e81539b1849cf04f070142
Michelle Yang <michelleyang@berkeley.edu>
2015-07-29 11:07:09 -0400

    modifying char

--- recurrent/char/test.py
+++ recurrent/char/test.py
@@ -3,12 +3,13 @@
 import nnet1d
 
 # Create model
-model = nnet1d.nnet1d.NNet1D(datafile="datasets/qri.pkl.gz", seed=42, batch_size=20,
+model = nnet1d.nnet1d.NNet1D(datafile="../../datasets/qri.pkl.gz", seed=42, batch_size=20,
                       learning_rate=0.01, momentum=0.99,
                       cost_fn=nnet1d.abs_error_cost)
 
 # Add layers and connect them
 model.add_recurrent_layer(output_length=500, activ_fn=nnet1d.relu)
+model.add_recurrent_layer()
 model.add_fully_connected_layer()
 model.build()
 
