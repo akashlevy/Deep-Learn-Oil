95172f17bb67fccf66c57869ac760650864ba716
Akash Levy <akashlevy@gmail.com>
2015-08-01 19:52:39 -0400

    Updated Keras models

--- keras/cnn.py
+++ keras/cnn.py
@@ -1,4 +1,4 @@
-"""Test convolutional neural network with Keras"""
+"""Test convolutional model with Keras"""
 
 import numpy as np
 import random
@@ -10,6 +10,9 @@ from keras.layers.convolutional import Convolution1D, MaxPooling1D
 from keras.models import Sequential
 from keras.optimizers import SGD
 
+
+# Model name
+MDL_NAME = "cnn"
 
 # Seed random number generator
 np.random.seed(42)
@@ -23,14 +26,10 @@ train_set, valid_set, test_set = datasets
 
 # Build neural network
 model = Sequential()
-model.add(Convolution1D(nb_filter=100, input_dim=1, filter_length=13))
-model.add(Activation("relu"))
+model.add(Convolution1D(1, 100, 13, activation="relu"))
 model.add(Dropout(0.5))
-# model.add(Convolution1D(nb_filter=10, input_dim=10, filter_length=13))
-# model.add(Activation("relu"))
-# model.add(MaxPooling1D(pool_length=4))
 model.add(Flatten())
-model.add(Dense(input_dim=3600, output_dim=12))
+model.add(Dense(3600, 12))
 
 # Use stochastic gradient descent and compile model
 sgd = SGD(lr=0.001, momentum=0.99, decay=1e-6, nesterov=True)
@@ -38,7 +37,7 @@ model.compile(loss=qri.mae_clip, optimizer=sgd)
 
 # Use early stopping and saving as callbacks
 early_stop = EarlyStopping(monitor='val_loss', patience=10)
-save_best = ModelCheckpoint("models/fcn.mdl", save_best_only=True)
+save_best = ModelCheckpoint("models/%s.mdl" % MDL_NAME, save_best_only=True)
 callbacks = [early_stop, save_best]
 
 # Train model
@@ -48,14 +47,19 @@ hist = model.fit(train_set[0], train_set[1], validation_data=valid_set,
 time_elapsed = time.time() - t0
 
 # Load best model
-model.load_weights("models/cnn.mdl")
+model.load_weights("models/%s.mdl" % MDL_NAME)
 
 # Print time elapsed and loss on testing dataset
+test_set_loss = model.test_on_batch(test_set[0], test_set[1])
 print "\nTime elapsed: %f s" % time_elapsed
-print "Testing set loss: %f" % model.test_on_batch(test_set[0], test_set[1])
+print "Testing set loss: %f" % test_set_loss
+
+# Save results
+qri.save_results("results/%s.out" % MDL_NAME, time_elapsed, test_set_loss)
+qri.save_history("models/%s.hist" % MDL_NAME, hist.history)
 
 # Plot training and validation loss
-qri.plot_train_valid_loss(hist)
+qri.plot_train_valid_loss(hist.history)
 
 # Make predictions
 qri.plot_test_predictions(model, train_set)
